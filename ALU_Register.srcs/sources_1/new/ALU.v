2b53c75e31c969ad2f822a3ff7986f6566b49c18
jeeshma tn <114424425+Jeeshmatn@users.noreply.github.com>
2025-01-16 12:13:42 +0530

    Update ALU.v

--- ALU_Register.srcs/sources_1/new/ALU.v
+++ ALU_Register.srcs/sources_1/new/ALU.v
@@ -2,8 +2,7 @@ module ALU(
 input [7:0]A,
 input [7:0]B,
 input  [2:0] ALU_OP,
-output reg [7:0]ALU_Out,
-output reg carry
+output reg [8:0]ALU_Out
     );
     
  always @(*)begin
@@ -12,38 +11,31 @@ output reg carry
      
     3'b000:begin
     ALU_Out= A + B;
-    carry=0;
     end
     
      3'b001:begin
      ALU_Out= (A - B);
-     carry=1;
      end
      
      3'b010:begin
      ALU_Out= (A & B);
-     carry=1;
      end
      
      3'b011:begin
      ALU_Out= (A | B);
-     carry=0;
     end
     
      3'b100:begin 
     ALU_Out=(A ^ B);
-     carry=1;
     end
     
      3'b101:begin
     ALU_Out= (A << 1);
-    carry=0;
     end
     
     
      3'b110: begin
     ALU_Out= A;   
-    carry=0;
     end
     
     endcase
