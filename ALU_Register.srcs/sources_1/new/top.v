13f78bd77ec738409d6e47c809812f2196060c27
jeeshma tn <114424425+Jeeshmatn@users.noreply.github.com>
2025-01-16 12:15:24 +0530

    Update top.v

--- ALU_Register.srcs/sources_1/new/top.v
+++ ALU_Register.srcs/sources_1/new/top.v
@@ -3,8 +3,7 @@ input Write_Enable,
 input Reg_Select,
 input [7:0]Data_In,
 input  [2:0] ALU_OP,
-output  [7:0]ALU_Out,
-output  carry
+output  [8:0]ALU_Out
     );
     
  wire [7:0]Reg1_Out;
@@ -13,7 +12,7 @@ output  carry
  Register Regmod(  .Write_Enable(Write_Enable) ,  .Reg_Select(Reg_Select)  , .Data_In(Data_In) , .Reg1_Out(Reg1_Out),
  .Reg2_Out(Reg2_Out) );   
  
- ALU ALUmod( .A(Reg1_Out), .B(Reg2_Out),  .ALU_OP(ALU_OP) ,  .ALU_Out(ALU_Out) , .carry(carry)  );
+ ALU ALUmod( .A(Reg1_Out), .B(Reg2_Out),  .ALU_OP(ALU_OP) ,  .ALU_Out(ALU_Out)   );
  
   
 endmodule
